bc528645d5edeecc550f1006aef63be1b10c1332
yuryofujita <dasman74@hotmail.com>
2020-08-31 19:55:11 +0900

    Rails 03 モデルについて理解する

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'pages/index'
   get 'pages/help'
-  get 'users/new'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
